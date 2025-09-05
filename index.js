19f6f0f207b415dde99fc58146b1f644f1bdc4a0
Alyssa Gramling <alyssagramling55@gmail.com>
2025-09-05 16:28:22 -0400

    completed vault codes

---
+++ index.js
@@ -0,0 +1,35 @@
+let code1;
+
+code1 = 7 + 3;
+
+console.log(code1);
+
+let code2;
+
+code2 = 120 / 2 - 30 + 10;
+
+console.log(code2);
+
+let code3;
+
+code3 = (400 % 35) + 24;
+
+console.log(code3);
+
+let message;
+
+message = "The vault has been secured. The combination is:";
+
+console.log(message);
+
+let codeA;
+
+codeA = code1 + "-" + code2 + "-" + code3;
+
+console.log(codeA);
+
+let codeB;
+
+codeB = `${code1}-${code2}-${code3}`;
+
+console.log(codeB);
